17b3e4583a679d57d387f08d4f5e2a86d1fab1f2
Thiago Bitencourt <thiago.bitencourt@senior.com.br>
2018-09-13 07:49:52 -0300

    Adding Application Java source

---
+++ src/main/java/clinic/programming/training/Application.java
@@ -0,0 +1,12 @@
+package clinic.programming.training;
+
+public class Application {
+    public Application() {
+        System.out.println("Inside Application");
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Starting Application");
+        Application app = new Application();
+    }
+}
